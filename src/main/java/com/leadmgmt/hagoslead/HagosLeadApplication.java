485df6c1063ed492f89df079acb2fcbdd7e9e6e4
Stevania Hagos <stevaniahagos@gmail.com>
2021-04-15 15:52:40 +0800

    initial commit

---
+++ src/main/java/com/leadmgmt/hagoslead/HagosLeadApplication.java
@@ -0,0 +1,13 @@
+package com.leadmgmt.hagoslead;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HagosLeadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HagosLeadApplication.class, args);
+	}
+
+}
